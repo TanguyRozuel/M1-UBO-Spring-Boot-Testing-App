c755d8df95ce9640dd2486d2ce3b08660bdebff4
Nymeloss <94916319+Nymeloss@users.noreply.github.com>
2022-03-15 14:58:29 +0100

    Update Peuplemente.sql

--- Peuplemente.sql
+++ Peuplemente.sql
@@ -2,9 +2,9 @@ INSERT INTO cours(intitule) VALUES ('STR');
 INSERT INTO cours(intitule) VALUES ('SOR');
 INSERT INTO cours(intitule) VALUES ('SI');
 
-INSERT INTO utilisateur(login,motDePasse,nom,prenom,mail) VALUES ('alorric,'mdp1','Lorric','Alexandre','alexandre.lorric@gmail.com');
-INSERT INTO utilisateur(login,motDePasse,nom,prenom,mail) VALUES ('trozuel,'mdp2','Rozuel','Tanguy','tanguy.rozuel@gmail.com');
-INSERT INTO utilisateur(login,motDePasse,nom,prenom,mail) VALUES ('akergus,'mdp3','Kergus','Axel','axel.kergus@gmail.com');
+INSERT INTO utilisateur(login,motDePasse,nom,prenom,mail) VALUES ('alorric','mdp1','Lorric','Alexandre','alexandre.lorric@gmail.com');
+INSERT INTO utilisateur(login,motDePasse,nom,prenom,mail) VALUES ('trozuel','mdp2','Rozuel','Tanguy','tanguy.rozuel@gmail.com');
+INSERT INTO utilisateur(login,motDePasse,nom,prenom,mail) VALUES ('akergus','mdp3','Kergus','Axel','axel.kergus@gmail.com');
 
 INSERT INTO vacataire(nom,prenom,mail) VALUES ('Lorric','Alexandre','alexandre.lorric@gmail.com');
 INSERT INTO vacataire(nom,prenom,mail) VALUES ('Rozuel','Tanguy','tanguy.rozuel@gmail.com');
